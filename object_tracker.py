79bed8dc1dacc36dcd2b21bc43d76aaf75d279b1
Kubiti <63860332+Kubiti@users.noreply.github.com>
2022-08-13 11:39:25 +0000

    memory growth

--- object_tracker.py
+++ object_tracker.py
@@ -44,6 +44,7 @@ def main(_argv):
 
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
+        tf.config.experimental.set_visible_devices(physical_devices[0], 'GPU')
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     if FLAGS.tiny:
